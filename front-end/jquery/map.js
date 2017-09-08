366a8bf6814560cdd3ecd0085cd6cb007144db6c
ellenfiscina <ellfiscina@gmail.com>
2017-09-08 18:38:04 -0300

    8 de setembro

--- front-end/jquery/map.js
+++ front-end/jquery/map.js
@@ -1,73 +1,111 @@
 $(document).ready(function(){
 	var json = JSON.parse('["usedroom", "usedroom", "emptyroom" ]');
 	var j;
-	$("#downli").hide();
-	$("#groundli").addClass("disabled");
+	$(".groundli").addClass("disabled");
 	
-	$("#one").click(function(){
-		$("#oneli").addClass("disabled");
-		$("#groundli, #twoli").removeClass("disabled");
-		$("#upli").show();
-		$("#downli").show();
-		
-		for (var i = 0; i < 44; i++) {
+	$(".one").click(function(){
+		$(".oneli").addClass("disabled");
+		$(".groundli, .twoli").removeClass("disabled");
+		$(".room").hide();
+		$(".gRoom").hide();
+		$(".gEmpty").hide();
+		$(".empty").hide();
+		$(".facility").hide();
+		$(".biblioteca").hide();
+		$(".lab").hide();
+
+		for (var i = 0; i < 38; i++) {
 			j = i+1;
-			if (i < 11) {
-				if(i == 9){
-					$('#sala1').append('<div class="empty"><p>Banheiro</p></div>');
+			if (i < 8) {
+				if(i == 6){
+					$('.sala1').append('<div class="empty"><p>Banheiro</p></div>');
 				}
 				else{
-					$('#sala1').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+					$('.sala1').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
 				}
 			} 
-			else if(i < 22){
-				if(i == 20){
-					$('#sala2').append('<div class="empty"><p>Escada</p></div>');
+			else if(i < 19){
+				if(i == 17){
+					$('.sala2').append('<div class="empty"><p>Escada</p></div>');
 				}
 				else{
-					$('#sala2').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+					$('.sala2').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
 				}
 			}
-			else if(i < 33){
-				if(i == 31){
-					$('#sala3').append('<div class="empty"><p>Banheiro</p></div>');
+			else if(i < 30){
+				if(i == 28){
+					$('.sala3').append('<div class="empty"><p>Banheiro</p></div>');
 				}
 				else{
-					$('#sala3').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+					$('.sala3').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
 				}
 			}
 			else{
-				if(i == 42){
-					$('#sala4').append('<div class="empty"><p>Escada</p></div>');
+				if(i == 36){
+					$('.sala4').append('<div class="empty"><p>Escada</p></div>');
 				}
 				else{
-					$('#sala4').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+					$('.sala4').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
 				}
 			}
 		}
 	});
-	$("#two").click(function(){
-		$("#twoli").addClass("disabled");
-		$("#upli").hide();
-		$("#groundli, #oneli").removeClass("disabled");
-		$("#downli").show();
+	$(".two").click(function(){
+		$(".twoli").addClass("disabled");
+		$(".groundli, .oneli").removeClass("disabled");
+		$(".gRoom").hide();
+		$(".gEmpty").hide();
+		$(".room").hide();
+		$(".empty").hide();
+		$(".facility").hide();
+		$(".biblioteca").hide();
+		$(".lab").hide();
+		j = 39;
+		for (var i = 0; i < 38; i++) {
+			if (i < 8) {
+				if(i == 6){
+					$('.sala1').append('<div class="empty"><p>Banheiro</p></div>');
+				}
+				else{
+					$('.sala1').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+				}
+			} 
+			else if(i < 19){
+				if(i == 17){
+					$('.sala2').append('<div class="empty"><p>Escada</p></div>');
+				}
+				else{
+					$('.sala2').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+				}
+			}
+			else if(i < 30){
+				if(i == 28){
+					$('.sala3').append('<div class="empty"><p>Banheiro</p></div>');
+				}
+				else{
+					$('.sala3').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+				}
+			}
+			else{
+				if(i == 36){
+					$('.sala4').append('<div class="empty"><p>Escada</p></div>');
+				}
+				else{
+					$('.sala4').append('<div class="room '+json[i]+'"><p>'+ j +'</p></div>');
+				}
+			}
+			j++;
+		}
 	});
-	$("#ground").click(function(){
-		$("#groundli").addClass("disabled");
-		$("#twoli, #oneli").removeClass("disabled");
-		$("#upli").show();
-		$("#downli").hide();
-	});
-	$("#up").click(function(){
-		$("#twoli").addClass("disabled");
-		$("#groundli, #oneli").removeClass("disabled");
-		$("#upli").hide();
-		$("#downli").show();
-	});
-	$("#down").click(function(){
-		$("#groundli").addClass("disabled");
-		$("#twoli, #oneli").removeClass("disabled");
-		$("#upli").show();
-		$("#downli").hide();
+	$(".ground").click(function(){
+		$(".groundli").addClass("disabled");
+		$(".twoli, .oneli").removeClass("disabled");
+		$(".gRoom").show();
+		$(".gEmpty").show();
+		$(".room").hide();
+		$(".empty").hide();
+		$(".facility").show();
+		$(".biblioteca").show();
+		$(".lab").show();
 	});
 });
