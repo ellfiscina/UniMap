5fe65cd39bf880d890d6fe22953551051ca8c2ad
joaopedrofn <joaopedrofn@gmail.com>
2017-10-02 17:51:31 -0300

    integração iniciada

---
+++ unimap/actions.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+spl_autoload_register(function ($class_name) {
+	    include $class_name . '.php';
+	});
+
+	Database::connect("localhost", "root", "", "unimap_teste");
+	$interface = new IFace();
+	switch($_GET["action"]){
+		case "login":
+			echo $interface->login($_POST["cpf"], $_POST["pass"]);
+			break;
+		case "showRooms":
+			echo $interface->showRooms();
+			break;
+		case "showSessionInfo":
+			echo $interface->showSessionInfo();
+			break;
+		case "showRoomSchedule":
+			echo $interface->showRoomSchedule($_GET["room"]);
+			break;
+		case "logout":
+			$interface->logout();
+			break;
+	}
+?>
