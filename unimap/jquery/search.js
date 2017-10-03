e5adc0bdaab43297e9133cefb02deb272a13185c
joaopedrofn <joaopedrofn@gmail.com>
2017-10-03 18:54:55 -0300

    nem sei o q dizer

--- unimap/jquery/search.js
+++ unimap/jquery/search.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 	$.getJSON("actions.php?action=search&term="+get, function(json){
-		console.log(json);
-		json["disciplines"].each(function(){});
+		$("#results").append("Disciplinas<hr>");
+		$(json["disciplines"]).each(function(){
+			console.log($(this));
+			$("#results").append("<a href='javascript:void(0);' id='"+$(this)[0]["cod_disc"]+"'>"+$(this)[0]["name"]+"</a><br>");
+		});
+		$("#results").append("<hr>Professores<hr>");
+		$(json["teachers"]).each(function(){
+			console.log($(this));
+			$("#results").append("<a href='javascript:void(0);' id='"+$(this)[0]["id"]+"'>"+$(this)[0]["name"]+"</a><br>");
+		});
 	});
 });
